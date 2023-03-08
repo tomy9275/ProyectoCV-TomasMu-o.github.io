bfe2f6c344db9d52dd80bd011b47c8d6558f3ccd
tomy9275 <126637145+tomy9275@users.noreply.github.com>
2023-03-07 22:47:48 -0300

    initial commit

---
+++ Proyecto Primer paso Front-end/js/funciones.js
@@ -0,0 +1,14 @@
+
+function mostrarDatosPersonales(){
+    document.getElementById("datosPersonales").style.display="flex"
+}
+ function mostrarEstudios(){
+    document.getElementById("estudios").style.display="flex"
+ }
+
+ function mostrarConocimientos(){
+    document.getElementById("conocimientos").style.display="flex"
+ }
+ function mostrarExperiencias(){
+    document.getElementById("experiencia").style.display="flex"
+ }
